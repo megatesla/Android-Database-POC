dcd89666a22bc3ed5c51d7797500ff634027ab0b
Paul Bunch <paulbunc@gmail.com>
2012-12-27 22:00:04 -0600

    Initial commit
    I'm moving the POC from Optimus to GitHub. It's fine if this is public,
    since this isn't the actual app.

---
+++ FoodDatabase/src/com/tesla/eventtracker/StudentDataOpenHelper.java
@@ -0,0 +1,5 @@
+package com.tesla.eventtracker;
+
+public class StudentDataOpenHelper
+{
+}
---
+++ FoodDatabase/src/com/tesla/eventtracker/StudentDataProvider.java
@@ -0,0 +1,36 @@
+package com.tesla.eventtracker;
+
+import android.content.*;
+import android.database.*;
+import android.net.*;
+
+public class StudentDatabase extends ContentProvider
+{
+	public StudentDatabase() {
+
+	}
+
+	public boolean onCreate() {
+		
+	}
+
+	public Cursor query() {
+		
+	}
+	
+	public Uri insert() {
+		
+	}
+	
+	public int delete() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public String getType() {
+		
+	}
+}

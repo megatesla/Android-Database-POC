ac5b784fbc847b6eda1a777ccb7890c1c23b7498
Paul Bunch <paulbunc@gmail.com>
2012-12-29 19:36:51 -0600

    StudentDataOpenHelper
    Thought I committed this just a second ago. Guess not?

--- FoodDatabase/src/com/tesla/eventtracker/StudentDataOpenHelper.java
+++ FoodDatabase/src/com/tesla/eventtracker/StudentDataOpenHelper.java
@@ -1,5 +1,32 @@
 package com.tesla.eventtracker;
 
-public class StudentDataOpenHelper
+import android.content.Context;
+import android.database.sqlite.*;
+import android.util.Log;
+
+public class StudentDataOpenHelper extends SQLiteOpenHelper
 {
+	public static final String TABLE_STUDENT_DATA = "studentdata";
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_NAME = "name";
+	public static final String COLUMN_GPA = "gpa";
+	
+	private static final String DATABASE_NAME = "studentdata.db";
+	private static final int DATABASE_VERSION = 1;
+	private static final String DATABASE_CREATE = "CREATE TABLE " + TABLE_STUDENT_DATA + "(" + COLUMN_ID + " " + "integer primary key autoincrement, "
+													+ COLUMN_NAME + " " + "varchar(255), " + COLUMN_GPA + " real);";
+	
+	public StudentDataOpenHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(DATABASE_CREATE);
+	}
+	
+	public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+		Log.w(StudentDataOpenHelper.class.getName(), "Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
+		database.execSQL("DROP TABLE IF EXISTS " + TABLE_STUDENT_DATA);
+		onCreate(database);
+	}
 }

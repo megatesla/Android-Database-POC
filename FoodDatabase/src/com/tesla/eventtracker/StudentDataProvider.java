c5d6d6de4f4c90a198ca3025b804308eb0c17e4f
Paul Bunch <paulbunc@gmail.com>
2013-01-01 12:11:22 -0600

    Renamed StudentDatabase
    Renamed it and added stubs for javadocs. Updated function arguments.

--- FoodDatabase/src/com/tesla/eventtracker/StudentDataProvider.java
+++ FoodDatabase/src/com/tesla/eventtracker/StudentDataProvider.java
@@ -3,34 +3,87 @@ package com.tesla.eventtracker;
 import android.content.*;
 import android.database.*;
 import android.net.*;
+import android.database.sqlite.*;
 
-public class StudentDatabase extends ContentProvider
+public class StudentDataProvider extends ContentProvider
 {
-	public StudentDatabase() {
-
+	private SQLiteDatabase database;
+	private StudentDataOpenHelper openHelper;
+	public StudentDataProvider() {
+		super();
 	}
 
+	/**
+	 * This instantiates the SQLiteOpenHelper for this provider.
+	 * It is called automatically when Android creates the provider
+	 * in response to a resolver request from a client.
+	 * @return A {@code boolean} indicating whether or not setup was successful.
+     */
+	@Override
 	public boolean onCreate() {
-		
+		openHelper = new StudentDataOpenHelper(getContext());
+		//Assumes that any errors will result in thrown exceptions.
+		return true;
 	}
 
-	public Cursor query() {
+	/**
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @return
+	 */
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 		
 	}
 	
-	public Uri insert() {
+	/**
+	 * @param
+	 * @param
+	 * @param
+	 * @return
+	 */
+	public Uri insert(Uri uri, ContentValues initialValues) {
 		
 	}
 	
-	public int delete() {
+	/**
+	 * @param
+	 * @param
+	 * @param
+	 * @return
+	 */
+	public int delete(Uri uri, String where, String[] whereArgs) {
 		
 	}
 	
-	public void update() {
+	/**
+	 * @param
+	 * @param
+	 * @param
+	 * @param
+	 * @return
+	 */
+	public void update(Uri uri, ContentValues values, String where, String[] whereArgs) {
 		
 	}
 	
-	public String getType() {
+	/**
+	 * @param
+	 * @return
+	 */
+	public String getType(Uri uri) {
+		
+	}
+	
+	/**
+	 * @param
+	 * @param
+	 * @return
+	 */
+	public String[] getStreamTypes(Uri uri, String mimeTypeFilter) {
 		
 	}
 }
